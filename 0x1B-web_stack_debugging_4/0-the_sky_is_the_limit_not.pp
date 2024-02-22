aca62a5bcc37ccc9d1e6bff02a784637375c4c2f
Mbukiseni <mbndima@gmail.com>
2024-02-22 03:11:16 -0800

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Increases number of requests that nginx can handle.
+# Increasing maximum open files limit for the nginx user.
+
+exec { 'set_ulimit_to 5000':
+  command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
+
+} -> exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+}
